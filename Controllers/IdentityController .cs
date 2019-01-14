56709a4ce7a308c8cd1c1f5d833272ca123ba5d5
daijinming <daijinming@live.cn>
2019-01-14 17:45:52 +0800

    Update IdentityController .cs

--- Controllers/IdentityController .cs
+++ Controllers/IdentityController .cs
@@ -7,14 +7,14 @@ using Microsoft.AspNetCore.Authorization;
 public class IdentityController : ControllerBase
 {   
 
-    //[Authorize(Roles = "admin")]
+    [Authorize(Roles = "admin")]
     [HttpGet]
     public IActionResult Get()
     {   
         return new JsonResult(from c in HttpContext.User.Claims select new { c.Type, c.Value });
     }
 
-    //[Authorize(Roles = "admin")]
+    [Authorize(Roles = "admin")]
     [Route("{id}")]
     [HttpGet]
     public string Get(int id)
